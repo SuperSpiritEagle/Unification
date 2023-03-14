f8f5192d76f259cfc6be42c4bdef95b8d87a420c
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-03-14 11:58:32 +0500

    init

---
+++ UnificationСollection/UnificationСollection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnificationСollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "1", "2", "1" };
+            string[] array2 = { "3", "2" };
+
+            List<string> collection = new List<string>();
+
+            Unification(array1, collection);
+            Unification(array2, collection);
+
+            Print(collection);
+        }
+
+        private static void Unification(string[] array, List<string> collection)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (collection.Contains(array[i]) == false)
+                {
+                    collection.Add(array[i]);
+                }
+            }
+        }
+
+        private static void Print(List<string> collection)
+        {
+            foreach (var number in collection)
+            {
+                Console.Write($"{number}");
+            }
+        }
+    }
+}
